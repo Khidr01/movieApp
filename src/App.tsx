d803c2a3133fa8734bb595e66c2c1cb3a8f277fb
Hidir Atasoy <220541304@firat.edu.tr>
2025-12-31 23:55:43 +0300

    Refactor App component to utilize MovieCard for rendering movie details

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { movies } from "./database/movies"
+import { MovieCard } from "./Components/MovieCard"
 
 function App() {
 
   return (
-    <>
-      {movies.map((movie) => {
-        return <li className="text-red-400" key={movie.id}>
-          <pre>
-          {movie.title}<br></br>
-          {movie.year}<br></br>
-          {movie.genre}<br></br>
-          {movie.director}<br></br>
-          {movie.rating}<br></br>
-          <img src={movie.poster}></img><br></br>
-          {movie.description}<br></br>
-          </pre>
-          <br></br>
+    <section className="grid grid-cols-4">
+      {movies.map((movies) => {
+        return <li className="text-red-400" key={movies.id}>
+          <MovieCard {...movies} />
         </li>
       })}
-    </>
+    </section>
   )
 }
 
