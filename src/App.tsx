75430dcb6aedd46660d324c5f3bf75fad2da2885
Hidir Atasoy <220541304@firat.edu.tr>
2025-12-31 19:22:49 +0300

    Creating simple card and render it with map for simplicity sake.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-
+import { movies } from "./database/movies"
 
 function App() {
 
   return (
     <>
-      
+      {movies.map((movie) => {
+        return <li className="text-red-400" key={movie.id}>
+          <pre>
+          {movie.title}<br></br>
+          {movie.year}<br></br>
+          {movie.genre}<br></br>
+          {movie.director}<br></br>
+          {movie.rating}<br></br>
+          <img src={movie.poster}></img><br></br>
+          {movie.description}<br></br>
+          </pre>
+          <br></br>
+        </li>
+      })}
     </>
   )
 }
